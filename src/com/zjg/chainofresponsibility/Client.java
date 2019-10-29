24e2a41296488b1c33d0cc4f6e8e4c0a496331ee
zjg <jinguo.zhang@yammii.com>
2019-10-29 16:49:01 +0800

    add test files

---
+++ src/com/zjg/chainofresponsibility/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.zjg.chainofresponsibility;
+
+/**
+ * @author apple
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/zjg/interpreterPattern/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.zjg.interpreterPattern;
+
+/**
+ * @author apple
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
